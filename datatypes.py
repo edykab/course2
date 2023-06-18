2c6dd0494c529528e726962c710625eb94e3872c
Edmond <kaboweiedmond@gmil.com>
2023-06-18 22:10:31 +0100

    Add new Python files

---
+++ datatypes.py
@@ -0,0 +1,39 @@
+# Integer
+x = 3
+type_x = type(x)
+print(f"The value of x is {x}, and the data type of x is {type_x}")
+
+# Float
+y = 3.14
+type_y = type(y)
+print(f"The value of y is {y}, and the data type of y is {type_y}")
+
+# Boolean
+z = True
+type_z = type(z)
+print(f"The value of z is {z}, and the data type of z is {type_z}")
+
+# String
+name = "John"
+type_name = type(name)
+print(f"The value of name is {name}, and the data type of name is {type_name}")
+
+# List
+my_list = [1, 2, 3]
+type_list = type(my_list)
+print(f"The value of my_list is {my_list}, and the data type of my_list is {type_list}")
+
+# Tuple
+my_tuple = (4, 5, 6)
+type_tuple = type(my_tuple)
+print(f"The value of my_tuple is {my_tuple}, and the data type of my_tuple is {type_tuple}")
+
+# Dictionary
+my_dict = {"name": "John", "age": 25}
+type_dict = type(my_dict)
+print(f"The value of my_dict is {my_dict}, and the data type of my_dict is {type_dict}")
+
+# Set
+my_set = {7, 8, 9}
+type_set = type(my_set)
+print(f"The value of my_set is {my_set}, and the data type of my_set is {type_set}")
---
+++ edmond_ifeanyi_kabowei.py
@@ -0,0 +1,6 @@
+first_name = "Edmond"
+middle_name = "Ifeanyi"
+last_name = "Kabowei"
+
+print (f"the first name of the user is {first_name}  the middle name is {middle_name} and the last name is {last_name}")
+
